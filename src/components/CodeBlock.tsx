db5a76d3c54da2dc4e6de6f66d77df247b54f7f2
wonseok-han <oshan1112@gmail.com>
2023-03-31 15:17:26 +0900

    CodeBlock eslint 관련 예외 추가

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import * as Prism from 'prismjs';
