cff3e8e97da8cdbcebb3d5ab471e47cdb2246b39
wonseok-han <oshan1112@gmail.com>
2023-03-31 15:16:41 +0900

    eslint 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import CodeBlock from 'components/CodeBlock';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <CodeBlock
+        language="javascript"
+        code={`function sum(a, b) {
+  return a + b;
+}`}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CodeBlock.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import * as Prism from 'prismjs';
+import { useRef, useEffect, useState } from 'react';
+
+import 'prismjs/themes/prism-tomorrow.css';
+
+interface CodeBlockProps {
+  code: string;
+  language: string;
+}
+
+const CodeBlock = ({ code, language }: CodeBlockProps) => {
+  const codeBlock = useRef<HTMLPreElement>(null);
+  const [copyText, setCopyText] = useState('Copy');
+
+  useEffect(() => {
+    if (codeBlock.current) {
+      codeBlock.current.innerHTML = Prism.highlight(
+        code,
+        Prism.languages[language],
+        language
+      );
+    }
+  }, [codeBlock, code, language]);
+
+  const handleCopyClick = () => {
+    if (codeBlock.current) {
+      const tempElement = document.createElement('textarea');
+      tempElement.value = code;
+      document.body.appendChild(tempElement);
+      tempElement.select();
+      navigator.clipboard.writeText(code);
+      document.body.removeChild(tempElement);
+
+      setCopyText('Copied');
+
+      setTimeout(() => {
+        setCopyText('Copy');
+      }, 1500);
+    }
+  };
+
+  return (
+    <div className="relative rounded-md overflow-hidden">
+      <div className="flex justify-between items-center bg-gray-700 text-white py-2 px-3">
+        <div>{language}</div>
+        <button
+          className="text-gray-300 hover:text-white"
+          onClick={handleCopyClick}
+        >
+          {copyText}
+        </button>
+      </div>
+      <pre
+        ref={codeBlock}
+        className="line-numbers rounded-none pre-scrollable p-4 bg-gray-800 text-white"
+        style={{ margin: 0 }}
+      >
+        <code className={`language-${language}`}>{code}</code>
+      </pre>
+    </div>
+  );
+};
+
+export default CodeBlock;
