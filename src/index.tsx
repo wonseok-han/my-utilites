98c62a84c32da3bbaaaec20a623b8648e6139462
wonseok-han <oshan1112@gmail.com>
2023-12-07 03:07:41 +0900

    react-snap 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,33 @@
 
 import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { hydrateRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter basename="/">
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
-);
+if (document.getElementById('root').hasChildNodes()) {
+  hydrateRoot(
+    document.getElementById('root'),
+    <React.StrictMode>
+      <HelmetProvider>
+        <BrowserRouter basename="/">
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+} else {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+  root.render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <BrowserRouter basename="/">
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+}
