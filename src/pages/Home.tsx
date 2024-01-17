389e09bb5316434c3cb911b125e2676dcd4f4e7e
wonseok-han <oshan1112@gmail.com>
2024-01-17 20:41:01 +0900

    Universal Link Href Test 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import ConvertString from './components/ConvertString';
+import UniversalLinkHrefTest from './components/UniversalLinkHrefTest';
 
 const Home = () => {
   return (
-    <div className="text-black">
-      <ConvertString />
-    </div>
+    <>
+      <div className="text-black">
+        <ConvertString />
+      </div>
+
+      <div className="text-black">
+        <UniversalLinkHrefTest />
+      </div>
+    </>
   );
 };
 
---
+++ src/pages/components/UniversalLinkHrefTest.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+
+import ContentTitle from 'components/ContentTitle';
+
+const UniversalLinkHrefTest = () => {
+  const [deepLink, setDeepLink] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.value;
+
+    setDeepLink(event.target.value);
+  };
+
+  const handleClick = () => {
+    location.assign(deepLink);
+  };
+
+  useEffect(() => {
+    if (deepLink) {
+      const element = document.createElement('a');
+      element.href = deepLink;
+      element.click();
+    }
+  }, [deepLink]);
+
+  return (
+    <>
+      <ContentTitle text="2. Universal Link Href Test" />
+      <div style={{ display: 'flex', gap: '5px' }}>
+        <input
+          style={{ width: '100%', padding: '10px 5px' }}
+          value={deepLink}
+          onChange={handleChange}
+        />
+        <button
+          style={{ border: '1px solid black', width: '100px' }}
+          type="button"
+          onClick={handleClick}
+        >
+          연결
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default UniversalLinkHrefTest;
